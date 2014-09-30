2a8a8e90b642bd649b2c001e5f0eb004e4a92fc1
Luc Aebischer <aebiluc@gmail.com>
2014-09-30 21:57:18 +0200

    Initialisation

---
+++ Main.cpp
@@ -0,0 +1,70 @@
+/* Fichier : Main.c
+* Auteur  : Luc Aebischer
+* Date    : 18.09.2014
+*
+* But     : Mini dictionnaire
+*
+*
+* Remarque(s) :
+*
+* Modifications : Date / Auteur / Raison
+*
+* Compilateur : Visual Studio 2013
+*/
+# define _CRT_SECURE_NO_WARNINGS
+
+# include <cstdio>
+# include <cstdlib>
+# include <iostream>
+# include <string>
+#include  <iomanip>
+
+using namespace std;
+
+
+int Menu(void);
+/*----------------------------------------------------------------------------*/
+int main(void)
+{
+
+	int choix;
+
+	cout << "Labo 1 \n";
+	cout << "Luc Aebischer\n\n";
+	
+	do
+	{
+		choix = Menu();
+		switch (choix)
+		{
+		case 0:
+			break;
+		case 1:
+
+			break;
+		default:
+			printf("Choix incorrect\n");
+			break;
+		}
+	} while (choix != 0);
+	return EXIT_SUCCESS;
+}
+int Menu(void)
+{
+	int choix;
+	cout << "========================MENU========================\n" ;
+	cout << "|1 - Lister les mots du dictionnaire                |\n" ;
+	cout << "|2 - Inserer un mot                                 |\n" ;
+	cout << "|3 - Supprimer un mot                               |\n" ;
+	cout << "|4 - Rechercher un mot francais	                    |\n" ;
+	cout << "|0 - Quitter                                        |\n" ;
+	cout << "====================================================\n" ;
+
+	cout << "Choix : ";
+	cin >> choix;
+	cin.clear();
+	cin.sync();
+	cout << endl << endl;
+	return choix;
+}
+
